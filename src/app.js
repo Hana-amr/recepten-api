339ea96f474bb5ce06ebe0b6fef641b89fa9f9b9
Hana-amr <hna_amr@outlook.com>
2026-01-01 00:55:45 +0100

    initiële express en mongodb instellingen

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`
+    <h1>Recepten API</h1>
+    <ul>
+      <li>GET /api/recipes</li>
+      <li>POST /api/recipes</li>
+    </ul>
+  `);
+});
+
+module.exports = app;
