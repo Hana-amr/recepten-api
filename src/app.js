bb753a3cbf82443c4ddfca7135c39d2d4b8b6794
Hana-amr <hna_amr@outlook.com>
2026-01-02 21:45:59 +0100

    Root API-documentatie verbeterd en uitgebreid

--- src/app.js
+++ src/app.js
@@ -9,30 +9,84 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send(`
     <h1>Recepten API</h1>
+    <p>
+      Deze API laat toe om recepten en ingrediënten te beheren.</p>
 
     <h2>Ingredients</h2>
     <ul>
-      <li>GET /api/ingredients</li>
-      <li>GET /api/ingredients/:id</li>
-      <li>POST /api/ingredients</li>
-      <li>PUT /api/ingredients/:id</li>
-      <li>DELETE /api/ingredients/:id</li>
+      <li>
+        <strong>GET /api/ingredients</strong><br>
+        Haalt een lijst van alle ingrediënten op.<br>
+        <p>Optionele query parameters:</p>
+        <ul>
+          <li>limit – maximum aantal resultaten</li>
+          <li>offset – startpositie voor paginatie</li>
+          <li>search – zoeken op naam</li>
+        </ul>
+        
+        <p>Voorbeelden:</p>
+        <ul>
+          <li>/api/ingredients?limit=5&offset=0</li>
+          <li>/api/ingredients?search=tom</li>
+        </ul>
+      </li>
+<br>
+      <li>
+        <strong>GET /api/ingredients/:id</strong><br>
+        Haalt één ingrediënt op via zijn unieke ID.
+      </li>
+
+      <li>
+        <strong>POST /api/ingredients</strong><br>
+        Voegt een nieuw ingrediënt toe.<br>
+        <pre>{
+  "name": "Tomaat",
+  "unit": "gram",
+  "calories": 18
+}</pre>
+      </li>
+
+      <li>
+        <strong>PUT /api/ingredients/:id</strong><br>
+        Past een bestaand ingrediënt aan.
+      </li>
+
+      <li>
+        <strong>DELETE /api/ingredients/:id</strong><br>
+        Verwijdert een ingrediënt.
+      </li>
     </ul>
 
     <h2>Recipes</h2>
     <ul>
-      <li>GET /api/recipes</li>
-      <li>GET /api/recipes/:id</li>
-      <li>POST /api/recipes</li>
-      <li>PUT /api/recipes/:id</li>
-      <li>DELETE /api/recipes/:id</li>
-      <li>GET /api/ingredients?limit=5&offset=0</li>
-      <li>GET /api/ingredients?search=tom</li>
+      <li>
+        <strong>GET /api/recipes</strong><br>
+        Haalt een lijst van alle recepten op.
+      </li>
+
+      <li>
+        <strong>GET /api/recipes/:id</strong><br>
+        Haalt één recept op inclusief gekoppelde ingrediënten.
+      </li>
+
+      <li>
+        <strong>POST /api/recipes</strong><br>
+        Voegt een nieuw recept toe met gekoppelde ingrediënten.
+      </li>
+
+      <li>
+        <strong>PUT /api/recipes/:id</strong><br>
+        Past een bestaand recept aan.
+      </li>
+
+      <li>
+        <strong>DELETE /api/recipes/:id</strong><br>
+        Verwijdert een recept.
+      </li>
     </ul>
   `);
 });
 
-app.use(express.static("public"));
 
 const ingredientRoutes = require("./routes/ingredientRoutes");
 app.use("/api/ingredients", ingredientRoutes);
